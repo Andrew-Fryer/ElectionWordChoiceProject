81147ca29a4baed1e6cdc7261a2967d9ba902181
Andrew Fryer <andrew.fryer@queensu.ca>
2020-11-24 15:48:02 -0500

    okay, now files are being read in properly

--- investigating.py
+++ investigating.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-speeches = pd.read_csv('speeches.csv')
-freq_word = pd.read_csv('mostfreq1000word.csv', encoding='utf8', quoting=3)
-freq_word_mat = pd.read_csv('mostfreq1000docword.csv')
-decp_word = pd.read_csv('deceptionword.csv')
-decp_word = pd.read_csv('deceptiondocword.csv')
+speeches = pd.read_csv('speeches.csv', header=None)
+winners = pd.read_csv('winners.csv', header=None)
+freq_word = pd.read_csv('mostfreq1000word.csv', header=None, encoding='utf8', quoting=3)
+freq_word_mat = pd.read_csv('mostfreq1000docword.csv', header=None)
+decp_word = pd.read_csv('deceptionword.csv', header=None)
+decp_word_mat = pd.read_csv('deceptiondocword.csv', header=None)
