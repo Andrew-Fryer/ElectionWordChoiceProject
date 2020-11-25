b7c80dac53f95b01b43215da72d771c3e06af3ad
Andrew Fryer <andrew.fryer@queensu.ca>
2020-11-25 14:36:40 -0500

    generate and save correlation matrix

--- investigating.py
+++ investigating.py
@@ -10,5 +10,14 @@ decp_word_list = decp_word[0].values.tolist()
 decp_word_mat = pd.read_csv('deceptiondocword.csv', header=None, names=decp_word_list)
 
 df = pd.concat([speeches, freq_word_mat, decp_word_mat, winners], axis=1)
-
 #df.to_csv('full_data.csv')
+
+correlation_matrix = df.corr()
+#correlation_matrix.to_csv('correlation_matrix.csv')
+
+import matplotlib.pyplot as plt
+
+plt.matshow(correlation_matrix)
+#plt.savefig('correlation_matrix.png')
+
+
