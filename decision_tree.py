3ab84e07dd811601d443de493c929d21432872d8
Andrew Fryer <andrew.fryer@queensu.ca>
2020-11-26 00:02:26 -0500

    add crappy decision tree

---
+++ decision_tree.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+from format_data import *
+
+df = pd.concat([elections, normalize(freq_word_mat), normalize(decp_word_mat), winners], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_by_election(df)
+
+clf = DecisionTreeClassifier().fit(X_train, y_train)
+
+score = clf.score(X_test, y_test)
+print(score)
